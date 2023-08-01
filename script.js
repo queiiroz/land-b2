b66f91dcc58930fc49c6c391040cdf890a17cb98
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2023-08-01 18:48:41 -0300

    construindo a eventose

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("ativo");
   NavMenu.classList.toggle("ativo");
 });
+
+const images = document.querySelectorAll(".slider");
+const size = images.length;
+let selectedIndex = 0;
+
+function next() {
+  images[selectedIndex].classList.remove("selected");
+  selectedIndex++;
+  if (selectedIndex >= size) {
+    selectedIndex = 0;
+  }
+  images[selectedIndex].classList.add("selected");
+}
+
+function prev() {
+  images[selectedIndex].classList.remove("selected");
+  selectedIndex--;
+  if (selectedIndex < 0) {
+    selectedIndex = size - 1;
+  }
+  images[selectedIndex].classList.add("selected");
+}
